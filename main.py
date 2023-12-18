0818e5c38d8627e3fae22874ca06f5545034b95a
Qaridasturchi <154114591+Qaridasturchi@users.noreply.github.com>
2023-12-18 10:23:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-from fastapi import FastAPI, HTTPException, Query
 from spotipy import *
-app = FastAPI()
 
-# Your existing functions
-def SearchFromSpotify(track_name, limit):
-    # ... (same as before)
 
-def DownloadMusic(UrlList):
-    # ... (same as before)
+def main():
+    track_name = input("Enter the track name: ")
+    limit = int(input("Enter the limit: "))
 
-# FastAPI endpoint to search and download music
-@app.get("/download-music/")
-async def download_music(track_name: str = Query(..., title="Track Name", description="Name of the track to search"),
-                         limit: int = Query(..., title="Limit", description="Limit the number of search results")):
-    try:
-        # Use the first function to search for the track on Spotify
-        track_urls = SearchFromSpotify(track_name, limit)
+    # Use the first function to search for the track on Spotify
+    track_urls = SearchFromSpotify(track_name, limit)
 
-        # Use the second function to download the music
-        audio_urls = DownloadMusic(track_urls)
+    # Use the second function to download the music
+    audio_urls = DownloadMusic(track_urls)
 
-        return {"downloaded_audio_urls": audio_urls}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    print("Downloaded audio URLs:")
+    for audio_info in audio_urls:
+        print(f"Name: {audio_info['name']}, URL: {audio_info['url']}")
+
+if name == "main":
+    main()
